90c75ba7d6c0c89f112a5c8c4e69434b292e5b3e
Nick Morgan <skilldrick@gmail.com>
2016-01-09 21:26:56 -0800

    Generate tunes and rhythms from fake sha

--- app/tune.js
+++ app/tune.js
@@ -1,43 +1,63 @@
 import {ctx} from './audio.js';
+import _ from 'underscore';
 
-const sha = '4739f5c1ddb71e212f1af2df667ef20ee2cc3ec5';
-const parts = sha.split('').map((num) => parseInt(num, 16));
+//const sha = '4739f5c1ddb71e212f1af2df667ef20ee2cc3ec5';
+//const parts = sha.split('').map((num) => parseInt(num, 16));
+//Generate random parts array
+const parts = Array.from(Array(40).keys()).map((val) => Math.floor(Math.random() * 16));
 
-const tune = parts.slice(0, 8);
+const tune = parts.slice(0, 11);
+const rhythmParts = parts.slice(11, 16);
 
-const C = 0;
-const C$ = 1;
-const Db = 1;
-const D = 2;
-const D$ = 3;
-const Eb = 3;
-const E = 4;
-const F = 5;
-const F$ = 6;
-const Gb = 6;
-const G = 7;
-const Ab = 8;
-const A = 9;
-const Bb = 10;
-const B = 11;
-const C2 = 12;
+const scalePart = parts[16];
+const transpose1 = parts[17];
+const transpose2 = parts[18];
 
-const scales = {
-  blues: [C, Eb, F, F$, G, Bb, C2],
-  jazzMin: [C, D, Eb, F, G, A, B, C2],
-  tritone: [C, Db, E, Gb, G, Bb, C2],
-  tritoneTwoSemi: [C, Db, D, F$, G, Ab, C2],
-  prometheus: [C, D, E, F$, A, Bb, C2],
-  oct1: [C, C$, D$, E, F$, G, A, Bb, C2],
-  oct2: [C, D, Eb, F, Gb, Ab, A, B, C2],
-  wholeTone: [C, D, E, F$, Ab, Bb, C2],
-  minPent: [C, Eb, F, G, Bb, C2],
-  majPent: [C, D, E, G, A, C2],
-  maj: [C, D, E, F, G, A, B, C2]
-};
+console.log('scale part', scalePart);
+console.log('transposes:', transpose1, transpose2);
 
-const playNote = (note, when, length) => {
-  const [osc, gain] = createOsc(noteToFreq(note));
+// Split each part of rhythmParts in two with bit masking and shifting
+const rhythm = _.flatten(rhythmParts.map((part) => [part & 3, part >> 2])).map((part) => part + 1);
+
+console.log(rhythm, tune);
+
+// Numbers of semitones from A
+const C = 3;
+const C$ = 4;
+const Db = 4;
+const D = 5;
+const D$ = 6;
+const Eb = 6;
+const E = 7;
+const F = 8;
+const F$ = 9;
+const Gb = 9;
+const G = 10;
+const Ab = 11;
+const A = 12;
+const Bb = 13;
+const B = 14;
+const C2 = 15;
+
+const scales = [
+  { name: 'Major pentatonic', scale: [C, D, E, G, A, C2] },
+  { name: 'Prometheus', scale: [C, D, E, F$, A, Bb, C2] },
+  { name: 'Blues', scale: [C, Eb, F, F$, G, Bb, C2] },
+  { name: 'Tritone', scale:   [C, Db, E, Gb, G, Bb, C2] },
+  { name: 'Major', scale: [C, D, E, F, G, A, B, C2] },
+  { name: 'Minor pentatonic', scale: [C, Eb, F, G, Bb, C2] },
+  { name: 'Two semitone tritone', scale: [C, Db, D, F$, G, Ab, C2] },
+  { name: 'Whole tone', scale: [C, D, E, F$, Ab, Bb, C2] }
+  /*
+  [C, D, Eb, F, G, A, B, C2], // jazz minor
+  [C, Db, E, F, Gb, Ab, B, C2], // persian
+  [C, D, Eb, F, Gb, Ab, A, B, C2], // octatonic 2
+  [C, C$, D$, E, F$, G, A, Bb, C2], // octatonic 1
+  */
+];
+
+const playNote = (note, when, length, type) => {
+  const [osc, gain] = createOsc(noteToFreq(note), type);
   const targetGain = 0.2;
   const fadeTime = 0.03;
 
@@ -50,38 +70,61 @@ const playNote = (note, when, length) => {
   osc.stop(when + length + fadeTime);
 };
 
-const createOsc = (frequency) => {
+const createOsc = (frequency, type) => {
   const gain = ctx.createGain();
   gain.gain.value = 0;
   gain.connect(ctx.destination);
 
   const osc = ctx.createOscillator();
-  osc.type = 'triangle';
+  osc.type = type;
   osc.frequency.value = frequency;
   osc.connect(gain);
 
   return [osc, gain];
 };
 
-const noteToFreq = (note) => 220 * Math.pow(2, note / 12);
+const noteToFreq = (note) => 110 * Math.pow(2, note / 12);
 
-/*
-const playScale = (scale) => {
-  scale.forEach((note, index) => playNote(note, ctx.currentTime + index / 2, 0.5));
+const get = (arr, index) => arr[index % arr.length];
+
+const getIndexAndLoops = (arr, index) => [index % arr.length, Math.floor(index / arr.length)];
+
+const sum = (arr) => arr.reduce((a, b) => a + b, 0);
+
+const getTimeAndLength = (arr, index) => {
+  const lengthOfNotes = sum(arr);
+  const [arrIndex, numLoops] = getIndexAndLoops(arr, index);
+  const length = arr[arrIndex];
+  const offset = sum(arr.slice(0, arrIndex));
+  return [offset + numLoops * lengthOfNotes, length];
 };
 
-playScale(scales.maj);
-*/
+const getNote = (scale, index) => {
+  const [arrIndex, numLoops] = getIndexAndLoops(scale, index);
+  return scale[arrIndex] + numLoops * 12;
+};
 
-const pickRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const scale = get(scales, scalePart);
+console.log(scale);
 
-for (let i = 0; i < 32; i++) {
-  for (let j = 0; j < 3; j++) {
-    playNote(pickRandom(scales.majPent), ctx.currentTime + i / 3, 1 / 3);
-  }
+const maybeReverse = (shouldReverse, arr) => shouldReverse ? arr.slice().reverse() : arr;
+
+// transpose: number of steps of the scale to transpose this note
+const playNoteAtIndex = (index, speed, type, transpose = 0, reverse = false) => {
+  const note = getNote(maybeReverse(reverse, scale.scale), get(tune, index + transpose));
+  const [when, length] = getTimeAndLength(maybeReverse(reverse, rhythm), index);
+  playNote(note, when / speed, length / speed, type);
 }
 
-console.log(pickRandom(scales.blues));
+for (let i = 0; i < 64; i++) {
+  if (i % 2 == 0) {
+    playNoteAtIndex(i / 2, 2, 'square', transpose1, true);
+  }
+  if (i % 4 == 0) {
+    playNoteAtIndex(i / 4, 1, 'triangle');
+  }
+  playNoteAtIndex(i, 4, 'sawtooth', transpose2);
+}
 
 
 
