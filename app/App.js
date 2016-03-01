54a61c9002435f90de1b1d6311a89c6ffc3b9a39
Nick Morgan <skilldrick@gmail.com>
2016-02-29 20:19:33 -0800

    Remove React (for now at least)

--- app/App.js
+++ app/App.js
@@ -1,15 +1,1 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
 import tune from './tune.js';
-
-class App extends Component {
-  render() {
-    return (
-      <small>
-        <a href="https://github.com/skilldrick/shasounds">github</a>
-      </small>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
