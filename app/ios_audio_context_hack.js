13d0f5d139a22371616a9c2d6b3faf768ea85821
Nick Morgan <nickm@twitter.com>
2016-03-01 22:40:00 -0800

    Clean up iOS AudioContext hack

--- app/ios_audio_context_hack.js
+++ app/ios_audio_context_hack.js
@@ -1,19 +1,28 @@
 import {ctx} from './audio.js';
 
+const runAfterAction = (func) => {
+  // Show instructions
+  const div = document.getElementById('ios-hack');
+  div.innerHTML = 'Tap screen to play';
+
+  // One-time handler function
+  const handler = () => {
+    func();
+    document.removeEventListener('touchend', handler, false);
+  };
+
+  // Run func on touchend
+  document.addEventListener('touchend', handler, false);
+};
+
 // iOS won't start the audio context until a user-initiated action
 const iOSAudioContextHack = (func) => {
-  // Try running the func
-  func();
-
   // If state is suspended, we're probably on iOS
   if (ctx.state === 'suspended') {
-    // Show instructions
-    const div = document.getElementById('ios-hack');
-    div.innerHTML = 'Tap screen to play';
-
-    // Run func on touchend
-    document.addEventListener('touchend', func, false);
+    runAfterAction(func);
+  } else {
+    func();
   }
-}
+};
 
 module.exports = {iOSAudioContextHack};
